dbbf8462fee0d4027489e9b5271ce48b35df91d2
omar hassan <ramo-3000@live.com>
2022-09-06 00:54:30 +0400

    new

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -116,29 +116,31 @@ const handelscroll = () => {
 
 
 
-  export const getStaticPaths =async () => {
-    const query = `*[_type == "product"] {slug{current}}`
-
-const products =await client.fetch(query)
-const paths = products.map((product)  => ({
-
-    params:{
-        slug: product.slug.current
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+    slug {
+      current
     }
-    
-    
-}))
+  }
+  `;
 
-return{
-    paths
-    ,fallback:'blocking'
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: { 
+      slug: product.slug.current
+    }
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
 }
 
-}
 
-
-export const getStaticProps = async({params}) => {
-  const query = `*[_type == "product" && slug.current == '${params.slug}'][0]`
+export const getStaticProps = async({params:{slug}}) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`
 
  
   const product = await client.fetch(query)
